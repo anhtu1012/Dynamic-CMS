5db4745f9677c4a1162e27f189f3e9babd3e648e
anhtu1012 <phamanhtu1012@gmail.com>
2025-12-02 14:59:10 +0700

    feat: add root reducer for Redux store

--- redux/RootReducer.ts
+++ redux/RootReducer.ts
@@ -1,12 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import authReducer from "./store/slices/loginSlice";
-import permissionsReducer from "./store/slices/permissions";
 import databaseReducer from "./store/slices/databaseSlice";
 // import darkModeReducer from "./store/slices/darkModeSlice";
 
 const rootReducer = combineReducers({
   auth: authReducer,
-  permissions: permissionsReducer,
   database: databaseReducer,
 });
 
